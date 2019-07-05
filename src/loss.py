cad174a984384ce43de328515033f7f66c91d7fa
MATT ZHAO <zhaomatt@amazon.com>
2019-07-04 18:06:10 -0700

    debugging prints for size of weights, targets, inputs

--- src/loss.py
+++ src/loss.py
@@ -7,5 +7,8 @@ class WeightedCrossEntropyLoss(torch.nn.modules.Module):
         self.Wt0 = Wt0
         
     def forward(self, inputs, targets, phase):
+        print(self.Wt1[phase].size(), self.Wt0[phase].size())
+        print(targets.size())
+        print(inputs.size())
         loss = - (self.Wt1[phase] * targets * inputs.log() + self.Wt0[phase] * (1 - targets) * (1 - inputs).log())
         return loss
