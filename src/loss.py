9b23ece4a31822c264efa5ca12f6817c44b13c2e
MATT ZHAO <zhaomatt@amazon.com>
2019-07-04 17:32:13 -0700

    Forgot to add loss function

---
+++ src/loss.py
@@ -0,0 +1,11 @@
+import torch
+
+class WeightedCrossEntropyLoss(torch.nn.modules.Module):
+    def __init__(self, Wt1, Wt0):
+        super(Loss, self).__init__()
+        self.Wt1 = Wt1
+        self.Wt0 = Wt0
+        
+    def forward(self, inputs, targets, phase):
+        loss = - (self.Wt1[phase] * targets * inputs.log() + self.Wt0[phase] * (1 - targets) * (1 - inputs).log())
+        return loss
